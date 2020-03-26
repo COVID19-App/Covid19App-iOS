89aee1c98b21e8cdb0e891adafdaf7c733f94c26
Cristian Palomino <cristian.palomino@scotiabank.com.pe>
2020-03-26 12:13:57 -0500

    Initial Commit

---
+++ covid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid
+//
+//  Created by Cristian Palomino on 3/26/20.
+//  Copyright © 2020 Cristian Palomino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
